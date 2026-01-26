db62ed320f68118f621c2caaed28de902ad484ff
smikton <smikton@gmail.com>
2026-01-26 18:12:44 +0100

    Create .eleventy.js

---
+++ my-blog/.eleventy.js
@@ -0,0 +1,22 @@
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("admin");
+  eleventyConfig.addPassthroughCopy("images");
+  eleventyConfig.addPassthroughCopy("_redirects");
+  
+  eleventyConfig.addCollection("posts", function(collectionApi) {
+    return collectionApi.getFilteredByGlob("posts/*.md").sort((a, b) => {
+      return b.date - a.date;
+    });
+  });
+  
+  return {
+    dir: {
+      input: ".",
+      output: "_site",
+      includes: "_includes",
+      data: "_data"
+    },
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk"
+  };
+};
